ebef6fafafe1d390a7634de0b7533ef0592f86af
Jeremy Wolfe <jpwolfe@me.com>
2022-03-03 20:08:19 -0500

    Added default function

--- Src/drv/drv_color.c
+++ Src/drv/drv_color.c
@@ -1,39 +1,35 @@
 /** @file 		drv_color.c
- *  @brief 
+ *  @brief
+ *  	This file is used to change the output color to the terminal
  *
- *  @author 	jeremywolfe
- *  @date 		Mar 2, 2022
+ *  @author 	Jeremy Wolfe
+ *  @date 		03 MAR 22
  *  @bug 
  */
 
+/* Includes */
 #include "board.h"
 
-void color(uint8_t color, uint8_t bf)
+/* Functions */
+
+/** @brief Changes color of print text
+ *
+ *  @param color The color you want to change to.
+ *  @param bf If you want bf or not
+ *  @return Void.
+ */
+void
+color(uint8_t color, uint8_t bf)
 {
 	printf("\033[%d;3%dm", bf, color);
 }
 
-//void yellow(void)
-//{
-//	printf("\033[1;33m");
-//}
-//
-//void yellow(void)
-//{
-//	printf("\033[1;33m");
-//}
-//
-//void yellow(void)
-//{
-//	printf("\033[1;33m");
-//}
-//
-//void yellow(void)
-//{
-//	printf("\033[1;33m");
-//}
-//
-//void yellow(void)
-//{
-//	printf("\033[1;33m");
-//}
+/** @brief Changes color of print back to white with no bf
+ *
+ *  @return Void.
+ */
+void
+colorDefault(void)
+{
+	printf("\033[0;37m");
+}
