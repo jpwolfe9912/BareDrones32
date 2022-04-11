b1ac3e7b79257db9e6003db5a0e67f72053e1ff1
Jpwolfe9912 <jpwolfe@me.com>
2022-04-11 13:35:48 -0400

    Deleted unused settings.

--- Src/Autodrone32.h
+++ Src/Autodrone32.h
@@ -82,53 +82,14 @@ typedef volatile uint8_t semaphore_t;
 
 typedef struct sensors_t
 {
-	float    accel500Hz[3];
-	float    accel100Hz[3];
-	float    attitude500Hz[3];
-	float    gyro500Hz[3];
+	float	accel500Hz[3];
+	float	accel100Hz[3];
+	float	attitude500Hz[3];
+	float	gyro500Hz[3];
+	float	attDeg500Hz[3];
 } sensors_t;
 
 extern sensors_t sensors;
-
-typedef struct gps_t
-{
-	int32_t  latitude;     // 1e-7 degrees
-	int32_t  longitude;    // 1e-7 degrees
-	int32_t  height;       // mm above ellipsoid
-	int32_t  hMSL;         // mm above mean sea level
-	int32_t  velN;         // cm/s
-	int32_t  velE;         // cm/s
-	int32_t  velD;         // cm/s
-	uint32_t speed;        // cm/s
-	uint32_t gSpeed;       // cm/s
-	int32_t  heading;      // deg 1e-5
-	uint8_t  numSats;
-	uint8_t  fix;
-	uint8_t  statusFlags;
-	uint32_t iTOW;         // mSec
-	uint16_t year;         // years
-	uint8_t  month;        // months
-	uint8_t  day;          // days
-	uint16_t hDop;
-	uint16_t vDop;
-	uint8_t  numCh;
-	uint8_t  chn[50];      // channel number
-	uint8_t  svid[50];     // satellite ID
-	uint8_t  cno[50];      // carrier to noise ratio (signal strength)
-	uint8_t  updated;
-} gps_t;
-
-extern gps_t gps;
-
-typedef struct homeData_t
-{
-	int32_t latitude;
-	int32_t longitude;
-	float   altitude;
-	float   magHeading;
-} homeData_t;
-
-extern homeData_t homeData;
 
 ///////////////////////////////////////////////////////////////////////////////
 // PID Definitions
@@ -181,8 +142,6 @@ typedef struct eepromConfig_t
 	float gyroTCBiasSlope[3];
 	float gyroTCBiasIntercept[3];
 
-	float magBias[6];
-
 	float accelCutoff;
 
 	float KpAcc;
@@ -215,48 +174,6 @@ typedef struct eepromConfig_t
 	///////////////////////////////////
 
 	PIDdata_t PID[NUMBER_OF_PIDS];
-
-	///////////////////////////////////
-
-	uint8_t osdEnabled;              // 0 = Disabled, 1 = Enabled
-	uint8_t defaultVideoStandard;    // 0 = NTSC, 1 = PAL
-	uint8_t metricUnits;             // 1 = metric
-
-	uint8_t osdDisplayAlt;           // 1 = Display OSD Altitude
-	uint8_t osdDisplayAltRow;
-	uint8_t osdDisplayAltCol;
-	uint8_t osdDisplayAltHoldState;  // 1 = display altitude hold state, 0 = don't display
-
-	uint8_t osdDisplayAH;            // 1 = Display OSD Artificial Horizon
-	uint8_t osdDisplayAtt;           // 1 = Display OSD Attitude
-
-	uint8_t osdDisplayHdg;           // 1 = Display OSD Heading
-	uint8_t osdDisplayHdgRow;
-	uint8_t osdDisplayHdgCol;
-
-	uint8_t osdDisplayHdgBar;	     // 1 = Display OSD Heading Bar (more visual)
-	uint8_t osdDisplayHdgBarRow;
-	uint8_t osdDisplayHdgBarCol;
-
-	uint8_t osdDisplayVoltage;	     // 1 = Display OSD Voltage
-	uint8_t osdDisplayVoltageRow;
-	uint8_t osdDisplayVoltageCol;
-
-	uint8_t osdDisplayCurrent;	     // 1 = Display OSD instantaneous current and used current
-	uint8_t osdDisplayCurrentRow;
-	uint8_t osdDisplayCurrentCol;
-
-	uint8_t osdDisplayThrot;	     // 1 = Display OSD throttle - for now, just rx input, maybe some day show rx input and processed throttle (alt,gps,etc)
-	uint8_t osdDisplayThrotRow;
-	uint8_t osdDisplayThrotCol;
-
-	uint8_t osdDisplayRSSI;          // 1 = Display OSD RSSI
-	uint8_t osdDisplayRSSIRow;
-	uint8_t osdDisplayRSSICol;
-
-	uint8_t osdDisplayTimer;
-	uint8_t osdDisplayTimerRow;
-	uint8_t osdDisplayTimerCol;
 
 	///////////////////////////////////
 
