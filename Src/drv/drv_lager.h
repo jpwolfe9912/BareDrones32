7923f83002fcc26ded1c7c3f886c3ba317468906
Jpwolfe9912 <jpwolfe@me.com>
2022-04-11 13:32:23 -0400

    Added files and functions for openlager.

--- Src/drv/drv_lager.h
+++ Src/drv/drv_lager.h
@@ -1,13 +1,16 @@
-/*
- * drv_lager.h
+/** @file 		drv_lager.h
+ *  @brief
+ *  	This file initializes the UART and provides functions for writing
+ *  	to the OpenLager.
  *
- *  Created on: Apr 7, 2022
- *      Author: jeremywolfe
+ *  @author 	Jeremy Wolfe
+ *  @date 		07 APR 2022
  */
 
 #ifndef SRC_DRV_DRV_LAGER_H_
 #define SRC_DRV_DRV_LAGER_H_
 
-
+void lagerInit(void);
+void lagerWriteLog(uint8_t *pLog);
 
 #endif /* SRC_DRV_DRV_LAGER_H_ */
