f1d130065761b408e6867a95a356f2714ede8e72
Jpwolfe9912 <jpwolfe@me.com>
2022-04-24 12:07:57 -0400

    Added options for PID tuning

--- Src/pid.h
+++ Src/pid.h
@@ -10,19 +10,42 @@
 #define __PID_H__
 
 /* Global Structs */
+#ifndef BAD_PID
 typedef struct PIDdata {
   float   P, I, D, Limit;
   float   integratorState;
   float   filterState;
   uint8_t prevResetState;
 } PIDdata_t;
+#else
+typedef struct fastPID{
+	float error;
+	float Kp;
+	float Ki;
+	float Kd;
+	float integral;
+	float derivative;
+	float last_error;
+	float pid_output;
+	float integral_limit;
+	float output_limit;
+}fastPID;
+#endif
 
 /* Global Variables */
 extern uint8_t pidReset;
+#ifdef BAD_PID
+extern fastPID attPIDdata;
+extern fastPID ratePIDdata;
+#endif
 
 /* Function Prototypes */
 void initPID(void);
+#ifndef BAD_PID
 float updatePID(float error, float deltaT, uint8_t reset, struct PIDdata *PIDparameters);
+#else
+float updatePID(struct fastPID *pidnow, float actual, float target);
+#endif
 void initPIDvalues(void);
 void setPIDstates(uint8_t IDPid, float value);
 void zeroPIDstates(void);
