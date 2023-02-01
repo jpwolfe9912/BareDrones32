224d6cd1aba778b3f4734fd2d80444130a46539f
Jeremy Wolfe <jpwolfe@me.com>
2023-01-31 21:19:45 -0600

    Removed comment

--- Src/main.c
+++ Src/main.c
@@ -12,7 +12,6 @@
 #ifndef SANDBOX
 /* Global Variables */
 const uint8_t __attribute__((__section__(".eeprom"), used)) eepromArray[131072];
-/* This is a test comment */
 
 eepromConfig_t eepromConfig;
 
